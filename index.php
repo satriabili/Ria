107d84dd4d47b46e8b3f93deb21a4b1dcb88c744
satriabili <165178752+satriabili@users.noreply.github.com>
2024-05-02 22:16:40 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curriculum Vitae</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="profile">
+            <img src="fotocv.jpg" alt="Profile Picture">
+            <h1>Satria Bili</h1>
+            <p>Software Developer</p>
+        </div>
+        <div class="content">
+            <h2>Tentang Saya</h2>
+            <p>Saya adalah seorang mahasiswa lulusan dari kampus IT di Bali yang suka membangun komunikasi yang baik antar sesama untuk membangun hubungan yang baik dengan klien saat saya bekerja.</p>
+
+            <h2>Pendidikan</h2>
+            <p>SD KATOLIK WAIKABUBAK 2, SMP KATOLIK WAIKABUBAK, SMA SWASTA KRISTEN WAIKABUBAK, S1 KOMPUTER (INSTITUT BISNIS DAN TEKNOLOGI INDONESIA)
+            </p>
+
+            <h2>Pengalaman</h2>
+            <p>Membuat Website</p>
+            <h2>Skills</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>PHP</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
